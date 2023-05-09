46be9b1fa4fe395835f6d45e53d1e77c4c133aa6
hazelfelt <hazelfelt@gmail.com>
2023-05-09 16:02:18 -0500

    figuring out uniform buffers

--- script.js
+++ script.js
@@ -21,14 +21,17 @@ context.configure({
 
 // Shader.
 const wgsl = `
-struct VertexOut {
-    @builtin(position) position: vec4f,
-    @location(12) color: vec4f,
+struct UniformStruct {
+    color: vec4f,
+    scale: vec2f,
+    offset: vec2f,
 };
+
+@group(0) @binding(0) var<uniform> uniform_struct: UniformStruct;
 
 @vertex fn vs(
     @builtin(vertex_index) index : u32
-) -> VertexOut {
+) -> @builtin(position) vec4f {
 
     var position = array<vec2f, 3>(
         vec2f( 0.0,  0.5),  // top center
@@ -36,23 +39,11 @@ struct VertexOut {
         vec2f( 0.5, -0.5)   // bottom right
     );
 
-    var color = array<vec4f, 3>(
-        vec4f(1.0, 0.0, 0.0, 1.0), // red
-        vec4f(0.0, 1.0, 0.0, 1.0), // green
-        vec4f(0.0, 0.0, 1.0, 1.0), // blue
-    );
-
-    var out: VertexOut;
-    out.position = vec4f(position[index], 0.0, 1.0);
-    out.color = color[index];
-
-    return out;
+    return vec4f(position[index] * uniform_struct.scale + uniform_struct.offset, 0.0, 1.0);
 }
 
-@fragment fn fs(input: VertexOut) -> @location(0) vec4f {
-    let grid_coords = vec2u(input.position.xy) / 32u;
-    let square_type = (grid_coords.x + grid_coords.y) % 2u == 0u;
-    return input.color * select(3.0, 2.0, square_type);
+@fragment fn fs(@builtin(position) input: vec4f) -> @location(0) vec4f {
+    return uniform_struct.color;
 }`;
 
 const module = device.createShaderModule({
@@ -75,6 +66,27 @@ const pipeline = device.createRenderPipeline({
     },
 });
 
+// Uniform buffer.
+const uniformBufferSize = 4*4 + 2*4 + 2*4;
+const uniformBuffer = device.createBuffer({
+    size: 4*4 + 2*4 + 2*4,
+    usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+});
+
+const uniformValues = new Float32Array(uniformBufferSize / 4);
+uniformValues.set([0, 1, 0, 1], 0); // color
+uniformValues.set([2.0, 2.0], 4) // scale
+uniformValues.set([-0.5, -0.25], 6); // offset
+device.queue.writeBuffer(uniformBuffer, 0, uniformValues);
+
+// Bind group.
+const bindGroup = device.createBindGroup({
+    layout: pipeline.getBindGroupLayout(0),
+    entries: [
+        { binding: 0, resource: { buffer: uniformBuffer }},
+    ],
+});
+
 // Command encoder and render pass.
 const encoder = device.createCommandEncoder({ label: 'encoder' });
 const pass = encoder.beginRenderPass({
@@ -92,6 +104,7 @@ const pass = encoder.beginRenderPass({
 });
 
 pass.setPipeline(pipeline);
+pass.setBindGroup(0, bindGroup);
 pass.draw(3);  // call our vertex shader 3 times
 pass.end();
 
